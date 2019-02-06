3349da99c51f47ac28ae81735e4ed2bd29262a2b
RusakYauheni <strengthofsupremacy@gmail.com>
2019-02-06 19:08:45 +0200

    upload files for showing in gh-page

---
+++ assets/js/script.signwebsite.js
@@ -0,0 +1,26 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2019. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+
+  'use strict';
+
+  $(function () {
+    // FastShell
+
+	$('.number-of-agents').on('change', function() {
+		$('.number-of-agents').not(this).prop('checked', false); 
+		 
+	});
+	$('.number-of-tickets').on('change', function() {
+		$('.number-of-tickets').not(this).prop('checked', false);  
+	});
+
+  });
+  	$('select').selectric();
+})(jQuery, window, document);
